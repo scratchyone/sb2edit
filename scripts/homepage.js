0a417ce87cdab6545110e7dfa907e1df670d894f
Scratchyone <scratchywon@gmail.com>
2016-05-14 09:46:38 -0400

    Fixed homepage html (ish)

---
+++ scripts/homepage.js
@@ -0,0 +1,11 @@
+function openfromurl() {
+document.getElementById("openfromurlbar").style.visibility=""
+if(!self.fetch) {
+    document.getElementById("errormodaltitle").innerHTML="Error: Fetch not found";
+    document.getElementById("errormodalmessage").innerHTML="Your browser does not support fetch! If you have a newer browser, please make an issue <a href='https://github.com/scratchyone/sb2edit/issues/new?title="+"Error: Fetch not found"+"&body="+encodeURIComponent(navigator.userAgent)+" ERROR: Fetch not found!''"+">here</a>. Please keep the prefilled data, and add anything else you would like.";
+    $("#open").closeModal();
+    $("#openurl").closeModal();
+    $("#error").openModal();
+}
+window.location = "/sb2edit/edit/?url=" + encodeURIComponent(document.getElementById("openurlfield").value)
+}
